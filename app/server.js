614663c55c7021396ed64cfde22b7492572c2c0a
indikaeranga <indikaeranga06@gmail.com>
2025-05-10 16:52:57 +0200

    new update

--- app/server.js
+++ app/server.js
@@ -1,20 +1,19 @@
-const http = require('http');
+const express = require('express');
 const os = require('os');
 
-const hostname = os.hostname();
-const ip = require('ip'); // For external IP
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`
-    <h1>Hello from Node.js applications</h1>
-    <p><strong>Hostname:</strong> ${hostname}</p>
-    <p><strong>IP Address:</strong> ${ip.address()}</p>
-  `);
+app.get('/', (req, res) => {
+  const info = {
+    hostname: os.hostname(),
+    platform: os.platform(),
+    arch: os.arch(),
+    ip: req.socket.localAddress
+  };
+  res.json(info);
 });
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server running at http://${ip.address()}:${port}/`);
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
