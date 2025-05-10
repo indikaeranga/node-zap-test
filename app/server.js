e829504b9d7f79324e8fdd49eeabcb4f947b3b87
indikaeranga <indikaeranga06@gmail.com>
2025-05-10 13:17:11 +0200

    editsevv

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end(`
-    <h1>Hello from Node.js</h1>
+    <h1>Hello from Node.js applications</h1>
     <p><strong>Hostname:</strong> ${hostname}</p>
     <p><strong>IP Address:</strong> ${ip.address()}</p>
   `);
